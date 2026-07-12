5da02da7909254fd02448f2378d8c21f787007d9
freddycardila <freddycardila@gmail.com>
2026-07-12 08:43:40 -0500

    Se cambia configuración de vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/previx/', // <-- AGREGA ESTA LÍNEA (incluye las barras diagonales)
+  base: './', // 
 })
